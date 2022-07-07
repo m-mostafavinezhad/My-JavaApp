46b0213262834d1b2ae9f5b2e2369d2d8e9a3ce4
m-mostafavinezhad <52644754+m-mostafavinezhad@users.noreply.github.com>
2022-07-07 04:59:35 +0430

    Add files via upload

---
+++ src/main/java/test/app/mvn/App.java
@@ -0,0 +1,13 @@
+package test.app.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
